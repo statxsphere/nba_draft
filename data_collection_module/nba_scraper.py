ae476dcd77c0be837c1ce342f56cd3f55304d01b
statxsphere <agarwalsshubham0@gmail.com>
2021-09-14 11:49:30 +0530

    changed file names and updated with finished code

--- data_collection_module/nba_scraper.py
+++ data_collection_module/nba_scraper.py
@@ -23,18 +23,24 @@ class NbaScrape:
         self.peridef = pd.DataFrame()
         self.mergeddf = pd.DataFrame()
         self.years = [a for a in range(2012, 2022)]
+        bas_url = f"https://www.nba.com/stats/players/traditional/?sort=PLAYER_NAME&dir=-1&Season={year - 1}-{str(year)[-2:]}&SeasonType=Regular%20Season"
+        self.driver = webdriver.Firefox()
+        self.driver.get(bas_url)
+        element = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "I Accept")]')))
+        element.click()
 
     def collect_touches(self, year):
         tch_url = f"https://www.nba.com/stats/players/touches/?Season={year-1}-{str(year)[-2:]}&SeasonType=Regular%20Season&sort=PLAYER_NAME&dir=-1"
-        driver = webdriver.Firefox()
-        driver.get(tch_url)
-        element = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "I Accept")]')))
-        element.click()
-        select = Select(driver.find_element_by_xpath(
+        # self.driver = webdriver.Firefox()
+        self.driver.get(tch_url)
+        WebDriverWait(self.driver, 2).until(
+            EC.element_to_be_clickable((By.XPATH, r"/html/body/main/div/div/div[2]/div/div/nba-stat-table/div[1]/div/div/select")))
+        # element.click()
+        select = Select(self.driver.find_element_by_xpath(
             r"/html/body/main/div/div/div[2]/div/div/nba-stat-table/div[1]/div/div/select"))
         select.select_by_index(0)
-        src = driver.page_source
+        src = self.driver.page_source
         soup = BeautifulSoup(src, "lxml")
         table = soup.find("div", attrs={"class": "nba-stat-table__overflow"})
         headers = table.findAll('th')
@@ -43,21 +49,20 @@ class NbaScrape:
         rows = table.findAll('tr')[1:]
         stats = [[td.getText().strip() for td in rows[i].findAll('td')] for i in range(len(rows))]
         touches = pd.DataFrame(stats, columns=headerlist)
-        touches.drop(columns=touches.columns[2:7], inplace=True)
         self.touches = self.touches.append(touches)
         del tch_url, touches
 
     def collect_passes(self, year):
         pas_url = f"https://www.nba.com/stats/players/passing/?Season={year-1}-{str(year)[-2:]}&SeasonType=Regular%20Season&sort=PLAYER_NAME&dir=-1"
-        driver = webdriver.Firefox()
-        driver.get(pas_url)
-        element = WebDriverWait(driver, 10).until(
+        # self.driver = webdriver.Firefox()
+        self.driver.get(pas_url)
+        WebDriverWait(self.driver, 2).until(
             EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "I Accept")]')))
-        element.click()
-        select = Select(driver.find_element_by_xpath(
+        # element.click()
+        select = Select(self.driver.find_element_by_xpath(
             r"/html/body/main/div/div/div[2]/div/div/nba-stat-table/div[1]/div/div/select"))
         select.select_by_index(0)
-        src = driver.page_source
+        src = self.driver.page_source
         soup = BeautifulSoup(src, "lxml")
         table = soup.find("div", attrs={"class": "nba-stat-table__overflow"})
         headers = table.findAll('th')
@@ -72,7 +77,28 @@ class NbaScrape:
         del pas_url, passes
 
     def collect_isolation(self, year):
-        pass
+        iso_url = f"https://www.nba.com/stats/players/isolation/?sort=PLAYER_NAME&dir=-1&SeasonType=Regular Season&SeasonYear={year-1}-{str(year)[-2:]}"
+        self.driver = webdriver.Firefox()
+        self.driver.get(iso_url)
+        element = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "I Accept")]')))
+        element.click()
+        select = Select(self.driver.find_element_by_xpath(
+            r"/html/body/main/div/div/div[2]/div/div/nba-stat-table/div[1]/div/div/select"))
+        select.select_by_index(0)
+        src = self.driver.page_source
+        soup = BeautifulSoup(src, "lxml")
+        table = soup.find("div", attrs={"class": "nba-stat-table__overflow"})
+        headers = table.findAll('th')
+        headerlist = [h.text.strip() for h in headers]
+        headerlist = [h.replace('\xa0', '') for h in headerlist]
+        rows = table.findAll('tr')[1:]
+        stats = [[td.getText().strip() for td in rows[i].findAll('td')] for i in range(len(rows))]
+        passes = pd.DataFrame(stats, columns=headerlist)
+        passes.drop(columns=passes.columns[2:7], inplace=True)
+        self.passes = self.passes.append(passes[['Player', 'Team', 'PassesMade', 'PassesReceived',
+                                                 'AST', 'SecondaryAST', 'AST', 'ASTToPass%']])
+        del iso_url, passes
 
     def final_call(self):
         pass
